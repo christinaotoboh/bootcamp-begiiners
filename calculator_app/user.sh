2c69d5e0b20bd4232c3ac40eacf9d16c0ff8b3ec
christinaotoboh <otobohsmiles@gmail.com>
2026-02-20 18:24:42 +0100

    Updated

---
+++ calculator_app/user.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Get user Inputs: Name and Surname
+# Use this to create a user with username -  namesurname@devopsclass.com
+# Assign the user sudo rights and display their name, surname and email
+
+# Collect user data
+  read -p "Please enter your first Name: " FIRST
+  read -p "Please enter your surame: " SURNAME
+  if [ -z "$FIRST" ] || [ -z "$SURNAME" ]; then
+    echo "A name field is missing, Names cannot be blank" 
+    exit
+  fi
+
+DOMAIN=@devopsclass.com
+EMAIL="$FIRST$SURNAME$DOMAIN"
+# create user in the /etc/passwd file
+#Assign user to sudo group
+# Print users details
+sudo useradd -mNG sudo $EMAIL 
+echo "Your name is: $FIRST"
+echo "Your surname is: $SURNAME"
+echo "Your email address is: $EMAIL"
+
+
+
+
+
+
+
+
+
+
+
+
